de2d1f4ea37d089469fdb5faaacc2ac7e73b6c25
Rahul Chaurasiya <rahulnag6896@gmail.com>
2022-06-26 15:18:37 +0530

    redux setup done

--- advance-todo/src/App.js
+++ advance-todo/src/App.js
@@ -1,9 +1,13 @@
-
+import Todo from './components/Todo'
+import {useEffect,useState} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {addTodo,removeTodo} from './service/actions/todoAction'
 
 function App() {
   return (
     <div className="App">
-      my app
+      my app      
+      <Todo/>
     </div>
   );
 }
